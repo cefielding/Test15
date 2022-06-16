e02e770e248b446a0ddead3ac51805bb7468114c
Catrin Fielding <catrinfielding@gmail.com>
2022-06-16 13:44:17 +0100

    Changed font size, words and colour

--- Test15/calc/views.py
+++ Test15/calc/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html', {'name':'Dan'})
+    return render(request,'home.html', {'name':'User'})
 
 def add(request):
 
